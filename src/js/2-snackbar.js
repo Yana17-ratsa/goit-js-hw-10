b5690212b66ccd0b618c57b36f62fdf93b5ea314
Yana17-ratsa <ratsayana13@gmail.com>
2024-03-27 15:42:07 +0200

    update 1, 2 tasks

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,35 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector(".form");
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const delay = parseInt(event.currentTarget.elements.delay.value);
+
+    const selectedBtn = event.currentTarget.querySelector('input[name="state"]:checked');
+
+    const state = selectedBtn ? selectedBtn.value : null;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(state === "fulfilled") {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    });
+
+    promise.then((delay) => {
+        iziToast.success({
+            title: 'OK',
+            message: `✅ Fulfilled promise in ${delay}ms`,
+        });
+    }).catch((delay) => {
+        iziToast.error({
+            title: 'Error',
+            message: `❌ Rejected promise in ${delay}ms`,
+        });
+    })
+})
